23a9277565f49a379ae2be74c9182e4fb39f07e9
Makiko Shukunobe <m_shukunobe@hotmail.com>
2019-04-22 12:30:13 -0400

    Delete run_server parameter.

--- app/run.py
+++ app/run.py
@@ -3,7 +3,7 @@ from Pathways.utils import Utils
 
 if __name__ == '__main__':
     INITIATE = True
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=True)
     U = Utils()
     if INITIATE:
         U.initDB()
